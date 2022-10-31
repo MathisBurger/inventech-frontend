376f2ed9272fa901e05ca624c61dd46553b0161d
Mathis <handymathis@gmail.com>
2022-10-31 22:24:13 +0100

    [TASK] Added links to webpage

--- components/TopNavbar.tsx
+++ components/TopNavbar.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 
+interface Page {
+    route: string;
+    name: string;
+}
 
 const TopNavbar: React.FC = () => {
+
+    const [pathName, setPathName] = useState<string|null>(null);
+
+    useEffect(() => {
+        setPathName(window.location.pathname);
+    });
+
+    const calculateActive = useCallback((route: string) => {
+        if (route === pathName) {
+            return "nav-item active";
+        } else {
+            return "nav-item";
+        }
+    }, [pathName]);
+
+    const pages: Page[] = [
+        {
+            route: '/',
+            name: 'Inventory'
+        },
+        {
+            route: '/reset-password',
+            name: 'Reset password'
+        }
+    ]
 
 
     return (
@@ -11,6 +40,15 @@ const TopNavbar: React.FC = () => {
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
+            <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav mr-auto">
+                    {pages.map((page) => (
+                        <li className={calculateActive(page.route)} key={page.route}>
+                            <a className="nav-link" href={page.route}>{page.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </nav>
     );
 }
